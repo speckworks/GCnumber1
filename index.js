315e1b57e67a75a2b8c2a9d374e0f4f60d6be3b7
John K. Speck <speckworks@gmail.com>
2020-06-07 21:57:47 +0000

    adjusting comments for clarity

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ function longestRow (arr){
     for(i = 0; i < length; i++) {
         // check the value of the current value against the last
         if(temp != null && temp == arr[i]) {
-            // it's a match! add 1 to the stretch
+            // you have a matching value add 1 to the stretch
             stretch++;
         } else {
-            // it's not a match, start stretch from 1 again
+            // it's not a matching value, start stretch from 1 again
             stretch = 1;
         }
         // set current value of the array being inspected for recurrence
         temp = arr[i];
 
-       // set the length of the longest stretch and the Value in stretch
+       // set the length of the longest stretch and the Value of that stretch
       if(stretch > longestStretch) {
         longestStretch = stretch;
         longestStretchValue = temp;
       }
     }
 
-    console.log("the length of the longest continuous stretch is:", longestStretch, "That Value is:", longestStretchValue);
+    console.log("The length of the longest continuous stretch is:", longestStretch, "places in the list of numbers.", "That Value is:", longestStretchValue);
 }
 
 longestRow(a)
